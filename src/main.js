73f6138c162f24e3c76df7cc7f6cc58c678bf2db
Алексей <Alexey@iMac-Anastasia.local>
2018-09-28 18:31:41 +0300

    add vue-router

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueGeolocation from 'vue-browser-geolocation'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 
-
+Vue.use(VueRouter)
 Vue.use(VueGeolocation)
 Vue.use(VueResource)
 
